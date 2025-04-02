457b38c3f4104f29d99788e29135e22b916e2f3f
MFBarker <98473836+MFBarker@users.noreply.github.com>
2025-04-02 12:07:03 -0600

    Player Controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,54 @@
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float speed = 3;
+    [SerializeField] float jumpheight = 2;
+    [SerializeField] LayerMask layerMask;
+
+    Rigidbody rb;
+    Vector3 force;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        Vector3 direction = Vector3.zero;
+        direction.x = Input.GetAxis("Horizontal");
+        direction.z = Input.GetAxis("Vertical");
+        force = direction * speed;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * Mathf.Sqrt(-2 * Physics.gravity.y * jumpheight), ForceMode.Impulse);
+        }
+
+        var colliders = Physics.OverlapSphere(transform.position, 2,layerMask);
+        foreach (var collider in colliders)
+        {
+            Destroy(collider.gameObject);
+        }
+
+        Debug.DrawRay(transform.position, transform.forward * 5, Color.red);
+        if (Physics.Raycast(transform.position, transform.forward, out RaycastHit hit, 5, layerMask))
+        {
+            Destroy(hit.collider.gameObject);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.AddForce(force,ForceMode.Force);
+        //rb.AddTorque(force);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, 2);
+    }
+}
---
+++ Assets/Scripts/PointEffector.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class PointEffector : MonoBehaviour
+{
+    //force
+    //radius (slider)
+
+    //push if positive, pull if negative
+    //two versions 
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
