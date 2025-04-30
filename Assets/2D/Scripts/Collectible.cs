cbada0b4b6ae7a4857671d9ff6dd3d0ec7e4ac79
MFBarker <98473836+MFBarker@users.noreply.github.com>
2025-04-29 22:11:00 -0600

    [A] Unity Game 2D - Part 1

--- Assets/2D/Scripts/Collectible.cs
+++ Assets/2D/Scripts/Collectible.cs
@@ -1,18 +1,26 @@
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-enum eType
+public enum eType
 { 
     Coin,
-    Gem
+    Gem,
+    Key
 }
 
 [RequireComponent(typeof(Collider2D))]
 public class Collectible : MonoBehaviour
 {
-    [SerializeField] eType type;
-    [SerializeField] IntDataSO Score;
-    [SerializeField] int val = 0;
-    
+    [Header("Data")]
+    [SerializeField] public eType type;
+    [SerializeField] public IntDataSO Score;
+    [SerializeField] public int val = 0;
+    [SerializeField] public bool destroyImmediately;
+
+    [Header("Input")]
+    [SerializeField] public InputActionReference interactInput;
+    [SerializeField] public EventChannelSO interactEvent;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -24,9 +32,52 @@ public class Collectible : MonoBehaviour
             case eType.Gem:
                 Score.Value += val;
                 break;
+            case eType.Key:
+                //
+                break;
         }
 
-        Destroy(this.gameObject);
+        if (destroyImmediately) Destroy(this.gameObject);
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (type == eType.Key) 
+        {
+            if (interactInput != null && interactInput.action.IsPressed())
+            {
+                interactEvent.Raise();
+                Destroy(this.gameObject);
+            }
+            
+        }
     }
 
 }
+
+//Custom Editor ()
+//[CustomEditor(typeof(Collectible))]
+//public class CollectibleEditor : Editor
+//{
+//    void OnInspectorGUI()
+//    {
+//        // the match object
+//        Collectible collectible = (Collectible)target;
+
+//        // display dropdown for gamemodes
+//        collectible.type = (eType)EditorGUILayout.EnumPopup("Gamemode", collectible.type);
+//        collectible.destroyImmediately = EditorGUILayout.("Gamemode", collectible.type);
+
+//        //Score Objects
+//        if (collectible.type == eType.Coin || collectible.type == eType.Gem)
+//        {
+//            collectible.val = EditorGUILayout.IntField("Value", collectible.val);
+//        }
+
+//        // Key
+//        if (collectible.type == eType.Key)
+//        {
+//            collectible.interactInput = EditorGUILayout.(collectible.interactInput);
+//        }
+//    }
+//}
--- Assets/2D/Scripts/GameManager.cs
+++ Assets/2D/Scripts/GameManager.cs
@@ -11,11 +11,13 @@ public class GameManager : MonoBehaviour
     [Header("Game UI")]
     [SerializeField] TMP_Text healthText;
     [SerializeField] TMP_Text scoreText;
+    [SerializeField] GameObject keyImage;
 
 
     private void Start()
     {
         //onPlayerDeath.AddListener(OnPlayerDeath);
+        if(keyImage.activeSelf) keyImage.SetActive(false);
     }
 
     private void FixedUpdate()
@@ -28,5 +30,15 @@ public class GameManager : MonoBehaviour
     public void OnPlayerDeath()
     {
         print("Player Dead");
+    }
+
+    public void onKeyGet()
+    { 
+        keyImage.SetActive(true);
+    }
+
+    public void onRespawn()
+    {
+        if (keyImage.activeSelf) keyImage.SetActive(false);
     }
 }
